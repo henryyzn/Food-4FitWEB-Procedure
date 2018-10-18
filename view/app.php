32e6edd4ac31e9d399939509fb3cca71ec17167b
HENRIQUE FRANCISCO DA SILVA <17170094@CFP127ALUNO.LOCAL>
2018-10-18 15:46:05 -0300

    Adaptação do projeto para o procedural

---
+++ view/app.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Food 4Fit</title>
+	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
+	<link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bases.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/colors.css">
+    <link rel="stylesheet" href="assets/css/font-style.css">
+    <link rel="stylesheet" href="assets/css/align.css">
+    <link rel="stylesheet" href="assets/css/keyframes.css">
+    <link rel="stylesheet" href="assets/css/mobile.css">
+	<script src="assets/public/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+</head>
+<body id="app-page">
+    <div class="app-page-wrapper">
+        <div class="column animate fadeInDown delay-500ms">
+            <figure>
+                <img src="assets/images/backgrounds/mockup.png" alt="Aplicativo Food 4Fit">
+            </figure>
+        </div>
+        <div class="column animate fadeInUp delay-1500ms">
+            <h2 class="padding-bottom-20px margin-left-30px">APLICATIVO<br />FOOD 4FIT</h2>
+            <p class="padding-bottom-40px margin-left-30px">CONTROLE DE DIETA<br />ACOMPANHAMENTO FITNESS<br />AGENDA<br />NOTIFICAÇÕES</p>
+            <span class="margin-left-30px">Disponível apenas para Android 5.0.1+</span>
+            <div class="btn-generic margin-top-10px margin-left-30px">
+                <span>Baixar</span>
+            </div>
+        </div>
+    </div>
+    <a href="index.php" class="app-page-back animate fadeInLeft delay-2500ms">Voltar</a>
+</body>
+</html>
---
+++ view/cms/components/sidebar.php
@@ -0,0 +1,105 @@
+<div id="sidebar">
+    <div id="sidebar-header">
+        <h1>FOOD 4FIT<br><span>CMS</span></h1>
+        <a href="#" id="sidebar-collapse">
+            <img src="../../assets/images/icons/menu-lateral.svg" alt="Menu Lateral">
+        </a>
+    </div>
+    <div class="separator"></div>
+    <div id="perfil">
+            <figure id="avatar">
+                <img src="../../assets/images/" alt="">
+            </figure>
+            <figure id="avatar"></figure>
+        <div id="informacoes">
+            <div id="informacoes-content">
+                <span id="nome"></span>
+                <span id="email"></span>
+                <a href="#" id="dropdown"></a>
+            </div>
+        </div>
+    </div>
+    <div class="separator"></div>
+    <nav>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/pagina-inicial.svg" alt="Dashboard"></span>
+            <span class="label">Dashboard</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/diario-de-bordo.svg" alt="Diário de Bordo"></span>
+            <span class="label">Diário de Bordo</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/pratos.svg" alt="Pratos"></span>
+            <span class="label">Pratos</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/ingredientes.svg" alt="Ingredientes"></span>
+            <span class="label">Ingredientes</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/categorias.svg" alt="Categorias de Pratos"></span>
+            <span class="label">Categorias de Pratos</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/categorias.svg" alt="Categorias de Ingredientes"></span>
+            <span class="label">Categorias de Ingredientes</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/marketing.svg" alt="Marketing"></span>
+            <span class="label">Marketing p/ E-Mail</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/clientes.svg" alt="Clientes"></span>
+            <span class="label">Clientes</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/niveis-de-acesso.svg" alt="Níveis de Acesso"></span>
+            <span class="label">Níveis de Acesso</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/pedidos.svg" alt="Pedidos"></span>
+            <span class="label">Pedidos<span class="badge">12</span></span>
+        </a>
+        <a href="#" data-page-load="fit-session">
+            <span class="image"><img src="../../assets/images/cms/icons/fit-session.svg" alt="Fit Session"></span>
+            <span class="label">Fit Session</span>
+        </a>
+        <a href="#" data-page-load="parceiros">
+            <span class="image"><img src="../../assets/images/cms/icons/parceiros.svg" alt="Parceiros"></span>
+            <span class="label">Parceiros</span>
+        </a>
+    </nav>
+    <div class="separator"></div>
+    <nav>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/meu-perfil.svg" alt="Meu perfil"></span>
+            <span class="label">Meu perfil</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/fale-conosco.svg" alt="Fale Conosco"></span>
+            <span class="label">Fale Conosco<span class="badge">12</span></span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/sobre-empresa.svg" alt="Sobre a Empresa"></span>
+            <span class="label">Sobre a Empresa</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/outras-funcoes.svg" alt="Outras Funções"></span>
+            <span class="label">Outras Funções</span>
+        </a>
+        <a href="#" data-page-load="lojas">
+            <span class="image"><img src="../../assets/images/cms/icons/diario-de-bordo.svg" alt="Diário de Bordo"></span>
+            <span class="label">Nossas Lojas</span>
+        </a>
+        <a href="#">
+            <span class="image"><img src="../../assets/images/cms/icons/ajuda.svg" alt="Ajuda"></span>
+            <span class="label">Ajuda</span>
+        </a>
+        <a href="#" class="btn-logout">
+            <span class="image"><img src="../../assets/images/cms/icons/sair.svg" alt="Sair"></span>
+            <span class="label">Sair</span>
+        </a>
+    </nav>
+    <div id="tooltip"></div>
+</div>
