b9b833a66c49b93b0fb0c626a2ee63c423524406
BARBARA THAYS SIQUEIRA DOS SANTOS SILVA <17170076@CFP127ALUNO.LOCAL>
2018-10-22 17:40:59 -0200

    Adaptando página APP site HTML/CSS

--- view/app.php
+++ view/app.php
@@ -5,24 +5,24 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Food 4Fit</title>
-	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
-	<link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/bases.css">
-    <link rel="stylesheet" href="assets/css/navbar.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/colors.css">
-    <link rel="stylesheet" href="assets/css/font-style.css">
-    <link rel="stylesheet" href="assets/css/align.css">
-    <link rel="stylesheet" href="assets/css/keyframes.css">
-    <link rel="stylesheet" href="assets/css/mobile.css">
-	<script src="assets/public/js/jquery-3.3.1.min.js"></script>
-	<script src="assets/js/scripts.js"></script>
+	<link rel="icon" type="image/png" href="../assets/images/icons/favicon.png"/>
+	<link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/bases.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    <link rel="stylesheet" href=../"assets/css/colors.css">
+    <link rel="stylesheet" href="../assets/css/font-style.css">
+    <link rel="stylesheet" href="../assets/css/align.css">
+    <link rel="stylesheet" href="../assets/css/keyframes.css">
+    <link rel="stylesheet" href="../assets/css/mobile.css">
+	<script src="../assets/public/js/jquery-3.3.1.min.js"></script>
+	<script src="../assets/js/scripts.js"></script>
 </head>
 <body id="app-page">
     <div class="app-page-wrapper">
         <div class="column animate fadeInDown delay-500ms">
             <figure>
-                <img src="assets/images/backgrounds/mockup.png" alt="Aplicativo Food 4Fit">
+                <img src="../assets/images/backgrounds/mockup.png" alt="Aplicativo Food 4Fit">
             </figure>
         </div>
         <div class="column animate fadeInUp delay-1500ms">
