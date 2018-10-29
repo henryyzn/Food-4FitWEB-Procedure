061c96301c51adc6f4f709663d46a47ae7a26b4f
LUIS HENRIQUE COUTINHO VIDAL <17170113@CFP127ALUNO.LOCAL>
2018-10-29 16:56:47 -0300

    pagina de desconto

---
+++ cms/models/DAO/descontoDAO.php
@@ -0,0 +1,54 @@
+<?php
+
+    class descontoDAO{
+        public function __construct(){
+            require_once('dataBase.php');
+            require_once('cms/models/descontoClass.php');
+        }
+
+        public function insert($classDesconto){
+            $sql = "insert into tbl_desconto(
+            id,
+            codig_cupom,
+            valor,
+            ativo,
+            validade) values (
+            '".$classDesconto->id."',
+            '".$classDesconto->codig_cupom."',
+            '".$classDesconto->valor."',
+            '".$classDesconto->ativo."',
+            '".$classDesconto->validade."'
+            );";
+
+            //INSTANCIA A CLASSE
+            $conex = new mysql_db();
+
+            //ABRE A CONEXÃO
+            $PDO_conex = $conex->conectar();
+
+            //EXECUTA A QUERY
+            if($PDO_conex->query($sql))
+                echo('Inseriu com sucesso');
+            else
+                echo('Erro no insert');
+
+            $conex->desconectar();
+        }
+
+        public function selectId(){
+
+        }
+
+        public function selectAll(){
+
+        }
+
+        public function delete(){
+
+        }
+
+        public function update(){
+
+        }
+    }
+?>
---
+++ cms/models/descontoClass.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Desconto{
+        public $id;
+        public $codig_cupom;
+        public $valor;
+        public $ativo;
+        public $validade;
+
+        public function __construct(){
+
+        }
+
+    }
+?>
