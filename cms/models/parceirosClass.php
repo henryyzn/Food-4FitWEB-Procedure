80b711c0cfbf7f9e8414980938b35d1836591909
LUIS HENRIQUE COUTINHO VIDAL <17170113@CFP127ALUNO.LOCAL>
2018-11-06 16:20:56 -0200

    parceirosDAO

---
+++ cms/models/parceirosClass.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Parceiros{
+        public $id;
+        public $id_usuario;
+        public $titulo;
+        public $descricao;
+        public $foto;
+        public $link;
+        public $ativo;
+
+
+
+
+
+    }
+?>
