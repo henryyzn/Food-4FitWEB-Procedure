4a8b16d2865c0439da5e5b365b5000a52a9ba5c1
Barbara Thays <bathays@hotmail.com>
2018-11-07 00:58:03 -0200

    Criando pastas, arquivos (vázios) para já editar. Feita a class e DAO do categorias-ingredientes.php

---
+++ cms/models/categorias-ingredientesClass.php
@@ -0,0 +1,14 @@
+<?php
+    class categoriaIngrediente{
+
+        public $id;
+        public $idCatIngredienteP;
+        public $titulo;
+        public $foto;
+        public $ativo;
+
+        public function __construct(){
+
+        }
+    }
+?>
