9037e2957c7e65987bc9faf037cd18e8ea9083df
henryyzn <henrique.franc18@gmail.com>
2018-11-19 01:04:33 -0200

    Banco definitivo

--- cms/models/DAO/dataBase.php
+++ cms/models/DAO/dataBase.php
@@ -13,10 +13,10 @@ class mysql_db
 
 	public function __construct(){
         //$this->server="10.107.144.250";
-        $this->server="localhost";
+        $this->server="142.44.189.41";
 		$this->user="root";
 //		$this->senha="bcd127";
-        $this->senha="";
+        $this->senha="mysql@2018";
         $this->dataBaseName="db_food4fit";
 
 	}
